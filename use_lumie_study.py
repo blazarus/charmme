d441bdfc6e7ddca504dc2a038acaa6fecd95adfb
Rob Speer <rspeer@mit.edu>
2011-05-06 16:08:22 -0400

    favor phrases

--- use_lumie_study.py
+++ use_lumie_study.py
@@ -3,10 +3,9 @@ import numpy as np
 import json
 import luminoso2
 
-model = luminoso2.load('pldb_luminoso2')
+model = luminoso2.load('pldb_2011_may')
 doc_matrix = model.get_doc_matrix('pldb')
 tag_matrix = model.get_tag_matrix()
-
 
 def get_related_people(vec, n=10):
     got = model.tags_similar_to_vector(vec)
@@ -21,7 +20,13 @@ def get_related_people(vec, n=10):
 
 def get_related_concepts(vec, n=10):
     got = model.domain_terms_similar_to_vector(vec)
-    return got.top_items(n)
+    return best_phrases(got, n)
+
+def best_phrases(simvec, n=10):
+    filtered = simvec.top_items(n*4)
+    favor_phrases = [(key, value*(len(key.split()))) for (key, value) in filtered]
+    favor_phrases.sort(key=lambda item: -item[1])
+    return favor_phrases[:n]
 
 def intersect_related_concepts(categories, n=10):
     if not isinstance(categories, list):
@@ -33,7 +38,7 @@ def intersect_related_concepts(categories, n=10):
     for category in categories[1:]:
         got = np.maximum(1e-6, model.domain_terms_similar_to_vector(category))
         prod = divisi2.multiply(prod, got)
-    return prod.top_items(n)
+    return best_phrases(prod, n)
         
 def make_user_vec(user):
     return tag_matrix.row_named(('person', user))
